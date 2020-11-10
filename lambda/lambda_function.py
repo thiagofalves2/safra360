29c3ee4005b86322f81f3cd9d760ca757003202a
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-10 15:11:20 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -45,10 +45,6 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
 class CaptureDocumentIntentHandler(AbstractRequestHandler):
     """Handler for Document Intent."""
-
-    def spellDigitOutput(number):
-        return "<say-as interpret-as=\"digits\">" + number + "</say-as>"
-
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         return ask_utils.is_intent_name("CaptureDocumentIntentHandler")(handler_input)
@@ -92,80 +88,6 @@ class CaptureDocumentIntentHandler(AbstractRequestHandler):
                 # .ask("add a reprompt if you want to keep the session open for the user to respond")
                 .response
         )
-
-# class HasBirthdayLaunchRequestHandler(AbstractRequestHandler):
-#     """Handler for launch after they have set their birthday"""
-#
-#     def can_handle(self, handler_input):
-#         # extract persistent attributes and check if they are all present
-#         attr = handler_input.attributes_manager.persistent_attributes
-#         attributes_are_present = ("year" in attr and "month" in attr and "day" in attr)
-#
-#         return attributes_are_present and ask_utils.is_request_type("LaunchRequest")(handler_input)
-#
-#     def handle(self, handler_input):
-#         attr = handler_input.attributes_manager.persistent_attributes
-#         year = int(attr['year'])
-#         month = attr['month'] # month is a string, and we need to convert it to a month index later
-#         day = int(attr['day'])
-#
-#         # get device id
-#         sys_object = handler_input.request_envelope.context.system
-#         device_id = sys_object.device.device_id
-#
-#         # get Alexa Settings API information
-#         api_endpoint = sys_object.api_endpoint
-#         api_access_token = sys_object.api_access_token
-#
-#         # construct systems api timezone url
-#         url = '{api_endpoint}/v2/devices/{device_id}/settings/System.timeZone'.format(api_endpoint=api_endpoint, device_id=device_id)
-#         headers = {'Authorization': 'Bearer ' + api_access_token}
-#
-#         userTimeZone = ""
-#         try:
-#             r = requests.get(url, headers=headers)
-#             res = r.json()
-#             logger.info("Device API result: {}".format(str(res)))
-#             userTimeZone = res
-#         except Exception:
-#             handler_input.response_builder.speak("There was a problem connecting to the service")
-#             return handler_input.response_builder.response
-#
-#         # getting the current date with the time
-#         now_time = datetime.now(timezone(userTimeZone))
-#
-#         # Removing the time from the date because it affects our difference calculation
-#         now_date = datetime(now_time.year, now_time.month, now_time.day)
-#         current_year = now_time.year
-#
-#         # getting the next birthday
-#         month_as_index = list(calendar.month_abbr).index(month[:3].title())
-#         next_birthday = datetime(current_year, month_as_index, day)
-#
-#         # check if we need to adjust bday by one year
-#         if now_date > next_birthday:
-#             next_birthday = datetime(
-#                 current_year + 1,
-#                 month_as_index,
-#                 day
-#             )
-#             current_year += 1
-#         # setting the default speak_output to Happy xth Birthday!!
-#         # Alexa will automatically correct the ordinal for you.
-#         # no need to worry about when to use st, th, rd
-#         speak_output = "Happy {}th birthday!".format(str(current_year - year))
-#         if now_date != next_birthday:
-#             diff_days = abs((now_date - next_birthday).days)
-#             speak_output = "Welcome back. It looks like there are \
-#                             {days} days until your {birthday_num}th\
-#                             birthday".format(
-#                                 days=diff_days,
-#                                 birthday_num=(current_year-year)
-#                             )
-#
-#         handler_input.response_builder.speak(speak_output)
-#
-#         return handler_input.response_builder.response
 
 class CaptureBirthdayIntentHandler(AbstractRequestHandler):
     """Handler for Hello World Intent."""
@@ -303,10 +225,8 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 
 sb = CustomSkillBuilder(persistence_adapter=s3_adapter)
 
-sb.add_request_handler(CaptureDocumentIntentHandler())
-# sb.add_request_handler(HasBirthdayLaunchRequestHandler())
 sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(CaptureBirthdayIntentHandler())
+sb.add_request_handler(CaptureDocumentIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
