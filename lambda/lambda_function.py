6907802ea742e5baeee4d739a9c02c5b9fbbfe35
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-11 00:36:44 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -196,27 +196,26 @@ class CaptureCelphoneIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         slots = handler_input.request_envelope.request.intent.slots
-        cpf_one = slots["cpf_one"].value
-        cpf_two = slots["cpf_two"].value
-        cpf_three = slots["cpf_three"].value
-        cpf_four = slots["cpf_four"].value
-        cpf_five = slots["cpf_five"].value
-        cpf_six = slots["cpf_six"].value
-        cpf_seven = slots["cpf_seven"].value
-        cpf_eight = slots["cpf_eight"].value
-        cpf_nine = slots["cpf_nine"].value
-        cpf_ten = slots["cpf_ten"].value
-        cpf_eleven = slots["cpf_eleven"].value
-        cpf = str(cpf_one) + str(cpf_two) + str(cpf_three) + "." + \
-            str(cpf_four) + str(cpf_five) + str(cpf_six) + "." + \
-            str(cpf_seven) + str(cpf_eight) + str(cpf_nine) + "-" + \
-            str(cpf_ten) + str(cpf_eleven)
+        celphone_area_one = slots["celphone_area_one"].value
+        celphone_area_two = slots["celphone_area_one"].value
+        celphone_digit = slots["celphone_digit"].value
+        celphone_number_one = slots["celphone_number_one"].value
+        celphone_number_two = slots["celphone_number_two"].value
+        celphone_number_three = slots["celphone_number_three"].value
+        celphone_number_four = slots["celphone_number_four"].value
+        celphone_number_five = slots["celphone_number_five"].value
+        celphone_number_six = slots["celphone_number_six"].value
+        celphone_number_seven = slots["celphone_number_seven"].value
+        celphone_number_eight = slots["celphone_number_eight"].value
+        celphone = "(" + str(celphone_area_one) + str(celphone_area_two) + ")" + str(celphone_digit) + \
+            str(celphone_number_one) + str(celphone_number_two) + str(celphone_number_three) + str(celphone_number_four) + "-" + \
+            str(celphone_number_five) + str(celphone_number_six) + str(celphone_number_seven) + str(celphone_number_eight)
         
         attributes_manager = handler_input.attributes_manager
         
-        # Add cpf variable to persisted attributes
+        # Add celphone variable to persisted attributes
         persisted_attributes = {
-            "cpf": cpf
+            "celphone": celphone
         }
         
         attributes_manager.persistent_attributes = persisted_attributes
@@ -224,8 +223,54 @@ class CaptureCelphoneIntentHandler(AbstractRequestHandler):
         # Save persisted attributes
         attributes_manager.save_persistent_attributes()
         
-        speak_output = 'Thanks, I will remember that your CPF is {cpf}. What\'s your celphone number?'.format(cpf=cpf)
-        reprompt_text = 'What\'s your celphone number?'
+        speak_output = 'Thanks, I will remember that your celphone is {celphone}. What\'s your account number?'.format(celphone=celphone)
+        reprompt_text = 'What\'s your account number?'
+        
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(reprompt_text)
+                .response
+        )
+
+class CaptureAccountIntentHandler(AbstractRequestHandler):
+    """Handler for Document Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("CaptureAccountIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        slots = handler_input.request_envelope.request.intent.slots
+        celphone_area_one = slots["celphone_area_one"].value
+        celphone_area_two = slots["celphone_area_one"].value
+        celphone_digit = slots["celphone_digit"].value
+        celphone_number_one = slots["celphone_number_one"].value
+        celphone_number_two = slots["celphone_number_two"].value
+        celphone_number_three = slots["celphone_number_three"].value
+        celphone_number_four = slots["celphone_number_four"].value
+        celphone_number_five = slots["celphone_number_five"].value
+        celphone_number_six = slots["celphone_number_six"].value
+        celphone_number_seven = slots["celphone_number_seven"].value
+        celphone_number_eight = slots["celphone_number_eight"].value
+        celphone = "(" + str(celphone_area_one) + str(celphone_area_two) + ")" + str(celphone_digit) + \
+            str(celphone_number_one) + str(celphone_number_two) + str(celphone_number_three) + str(celphone_number_four) + "-" + \
+            str(celphone_number_five) + str(celphone_number_six) + str(celphone_number_seven) + str(celphone_number_eight)
+        
+        attributes_manager = handler_input.attributes_manager
+        
+        # Add celphone variable to persisted attributes
+        persisted_attributes = {
+            "celphone": celphone
+        }
+        
+        attributes_manager.persistent_attributes = persisted_attributes
+        
+        # Save persisted attributes
+        attributes_manager.save_persistent_attributes()
+        
+        speak_output = 'Thanks, I will remember that your celphone is {celphone}. What\'s your account number?'.format(celphone=celphone)
+        reprompt_text = 'What\'s your account number?'
         
         return (
             handler_input.response_builder
@@ -343,6 +388,7 @@ sb.add_request_handler(BankingIntentHandler())
 sb.add_request_handler(AccountIntentHandler())
 sb.add_request_handler(CaptureCPFIntentHandler())
 sb.add_request_handler(CaptureCelphoneIntentHandler())
+sb.add_request_handler(CaptureAccountIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
