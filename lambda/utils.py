ea37c6a83bf02054e4601472859771bc2c8e5264
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-12 02:03:23 +0000

    updated lambda/utils.py.

--- lambda/utils.py
+++ lambda/utils.py
@@ -1,8 +1,10 @@
 import logging
 import os
 import boto3
+import requests
+from requests import Request, Session
 from botocore.exceptions import ClientError
-
+from ask_sdk_model import Response
 
 def create_presigned_url(object_name):
     """Generate a presigned URL to share an S3 object with a capped expiration of 60 seconds
@@ -25,3 +27,51 @@ def create_presigned_url(object_name):
 
     # The response contains the presigned URL
     return response
+
+""" Function to call API token. """
+def get_token():
+    token_host = "https://idcs-902a944ff6854c5fbe94750e48d66be5.identity.oraclecloud.com"
+    token_key = "OThiMmEwZDY0MWQ0NDFmZDhmMWQyOTdlNDg3NjFmMzk6ZDczMjU5YWYtYzJhZC00MTMzLWI0NjEtNDYyN2IwN2VlMDZj"
+    token_body = "grant_type=client_credentials&scope=urn:opc:resource:consumer::all"
+    token_url = '{}/oauth2/v1/token'.format(token_host)
+    token_headers = {'Authorization': 'Basic ' + token_key, 'Content-Type': 'application/x-www-form-urlencoded'}
+    
+    try:
+        request_token = requests.post(token_url, headers=token_headers, data=token_body)
+        response_token = request_token.json()
+        response_token_status = response_token.status_code
+        logger.info("Token API status code: {}".format(response_token_status))
+    except Exception:
+        handler_input.response_builder.speak("There was a problem connecting to the Token API.")
+        return handler_input.response_builder.response
+        
+    token = response_token['access_token']
+    logger.info("Token API result: {}".format(token))
+    
+    return token
+
+""" Function to call Safra API. """
+def call_safra_api(option = ''):
+    safra_host = "https://af3tqle6wgdocsdirzlfrq7w5m.apigateway.sa-saopaulo-1.oci.customer-oci.com/fiap-sandbox"
+    safra_url = '{safra_host}/open-banking/v1/accounts/{persisted_account_number}{option}'.format(safra_host=safra_host,endpoint=endpoint,persisted_account_number=persisted_account_number,option=option)
+    
+    token = get_token()
+    
+    safra_headers = {'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}
+    
+    #session = Session()
+    #prepped = Request('GET', safra_url, headers=safra_headers).prepare()
+    #r_safra = session.send(prepped)
+    #r_safra_status_code = r_safra.status_code
+    
+    try:
+        request_safra = requests.get(safra_url, headers=safra_headers)
+        response_safra = request_safra.json()
+        response_safra_status = response_safra.status_code
+        logger.info("Safra API status code: {}".format(response_safra_status))
+        logger.info("Safra API result: {}".format(str(response_safra)))
+    except Exception:
+        handler_input.response_builder.speak("There was a problem connecting to the Safra API.")
+        return handler_input.response_builder.response
+    
+    return response_safra
