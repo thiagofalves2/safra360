5eecb2eb90ea90390f575c6f0cf4ba7e6af0ce5c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-12 03:57:51 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -219,59 +219,86 @@ class AccountIntentHandler(AbstractRequestHandler):
             logger.info("Account Data: {}".format(account_data))
             
             transaction = account_data['transaction']
-            balance_record = balance[0]
+            transaction_record = transaction[0]
             
-            account_id = balance_record['AccountId']
+            account_id = transaction_record['accountId']
             logger.info("Account Id: {}".format(account_id))
             
-            amount_record = balance_record['Amount']
+            transaction_id = transaction_record['transactionId']
+            logger.info("Transaction Id: {}".format(transaction_id))
             
-            amount = amount_record['Amount']
-            logger.info("Balance Amount: {}".format(amount))
+            transaction_amount = transaction_record['amount']['amount']
+            logger.info("Transaction Amount: {}".format(transaction_amount))
             
-            currency = amount_record['Currency']
-            logger.info("Balance Currency: {}".format(currency))
+            transaction_currency = transaction_record['amount']['currency']
+            logger.info("Transaction Currency: {}".format(transaction_currency))
             
-            credit_debit = balance_record['CreditDebitIndicator']
+            credit_debit = transaction_record['creditDebitIndicator']
             logger.info("Credit/Debit: {}".format(credit_debit))
             
-            balance_type = balance_record['Type']
-            logger.info("Balance Type: {}".format(balance_type))
+            transaction_status = transaction_record['status']
+            logger.info("Transaction Status: {}".format(transaction_status))
             
-            balance_date = balance_record['DateTime']
-            logger.info("Balance Date: {}".format(balance_date))
+            transaction_booking_datetime = transaction_record['bookingDateTime']
+            logger.info("Transaction Booking Date: {}".format(transaction_booking_datetime))
             
-            balance_credit_line = balance_record['CreditLine']
-            credit_line_record = balance_credit_line[0]
+            transaction_value_datetime = transaction_record['valueDateTime']
+            logger.info("Transaction Value Date: {}".format(transaction_value_datetime))
             
-            credit_line_included = credit_line_record['Included']
-            logger.info("Credit line included? {}".format(credit_line_included))
+            transaction_info = transaction_record['transactionInformation']
+            logger.info("Transaction Info: {}".format(transaction_info))
             
-            credit_line_amount = credit_line_record['Amount']['Amount']
-            logger.info("Credit Line Amount: {}".format(credit_line_amount))
-
-            credit_currency = credit_line_record['Amount']['Currency']
-            logger.info("Credit Line Currency: {}".format(credit_currency))
+            bank_transaction_code = transaction_record['bankTransactionCode']['code']
+            logger.info("Bank Transaction Code: {}".format(bank_transaction_code))
             
-            credit_line_type = credit_line_record['Type']
-            logger.info("Credit Line Type: {}".format(credit_line_type))
+            bank_transaction_subcode = transaction_record['bankTransactionCode']['subCode']
+            logger.info("Bank Transaction Subcode: {}".format(bank_transaction_subcode))
+            
+            proprietary_bank_transaction_code = transaction_record['proprietaryBankTransactionCode']['code']
+            logger.info("Proprietary Bank Transaction Code: {}".format(proprietary_bank_transaction_code))
+            
+            proprietary_bank_transaction_issuer = transaction_record['proprietaryBankTransactionCode']['issuer']
+            logger.info("Proprietary Bank Transaction Issuer: {}".format(proprietary_bank_transaction_issuer))
+            
+            transaction_balance_amount_record = transaction_record['balance']['amount']
+            
+            transaction_balance_amount = transaction_balance_amount_record['amount']
+            logger.info("Transaction Balance Amount: {}".format(transaction_balance_amount))
+            
+            transaction_balance_currency = transaction_balance_amount_record['currency']
+            logger.info("Transaction Balance Currency: {}".format(transaction_balance_currency))
+            
+            transaction_balance_creditdebit = transaction_record['balance']['creditDebitIndicator']
+            logger.info("Transaction Balance Credit/Debit: {}".format(transaction_balance_creditdebit))
+            
+            transaction_balance_type = transaction_record['balance']['type']
+            logger.info("Transaction Balance Type: {}".format(transaction_balance_type))
             
             account_link = response_safra['Links']['Self']
 
-            speak_output = 'Your account balance is: \
+            speak_output = 'Here\'s your account transaction: \
                 Account ID: {account_id} \
-                Balance Amount: {amount} \
-                Balance Currency: {currency} \
-                Credit/Debit: {credit_debit} \
-                Balance Type: {balance_type} \
-                Balance Date: {balance_date} \
-                Credit Line Included? {credit_line_included} \
-                Credit Line Amount: {credit_line_amount} \
-                Credit Line Currency: {credit_currency} \
-                Credit Line Type: {credit_line_type} \
-                Link: {account_link}'.format(account_id=account_id,amount=amount,currency=currency, \
-                credit_debit=credit_debit,balance_type=balance_type,balance_date=balance_date,credit_line_included=credit_line_included, \
-                credit_line_amount=credit_line_amount, credit_currency=credit_currency, credit_line_type=credit_line_type, account_link=account_link)
+                Transaction ID: {transaction_id} \
+                Transaction Amount: {transaction_amount} \
+                Transaction Currency: {transaction_currency} \
+                Transaction Credit/Debit: {credit_debit} \
+                Transaction Status: {transaction_status} \
+                Transaction Booking Date: {transaction_booking_datetime} \
+                Transaction Value Date: {transaction_value_datetime} \
+                Transaction Info: {transaction_info} \
+                Bank Transaction Code: {bank_transaction_code} \
+                Bank Transaction Subcode: {bank_transaction_subcode} \
+                Proprietary Bank Transaction Code: {proprietary_bank_transaction_code} \
+                Proprietary Bank Transaction Issuer: {proprietary_bank_transaction_issuer} \
+                Transaction Balance Amount: {transaction_balance_amount} \
+                Transaction Balance Currency: {transaction_balance_currency} \
+                Transaction Balance Credit/Debit: {transaction_balance_creditdebit} \
+                Transaction Balance Type: {transaction_balance_type} \
+                Link: {account_link}'.format(account_id=account_id,transaction_id=transaction_id,transaction_currency=transaction_currency, \
+                credit_debit=credit_debit,transaction_status=transaction_status,transaction_booking_datetime=transaction_booking_datetime,transaction_value_datetime=transaction_value_datetime, \
+                transaction_info=transaction_info, bank_transaction_code=bank_transaction_code, bank_transaction_subcode=bank_transaction_subcode, proprietary_bank_transaction_code=proprietary_bank_transaction_code, \
+                proprietary_bank_transaction_issuer=proprietary_bank_transaction_issuer, transaction_balance_amount=transaction_balance_amount, transaction_balance_currency=transaction_balance_currency, \
+                transaction_balance_creditdebit=transaction_balance_creditdebit, transaction_balance_type=transaction_balance_type, account_link=account_link)
         
         # It will exit for now.
         return (
