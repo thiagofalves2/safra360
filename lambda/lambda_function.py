4af1f5e85bc1da3ea3ec5d3db455c2ac00de8166
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-12 04:59:36 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -64,28 +64,6 @@ class HasClientInfoLaunchRequestHandler(AbstractRequestHandler):
         persisted_celphone = attr['celphone']
         persisted_account_number = attr['account_number']
         
-        #speak_output = 'Welcome back, your CPF is {persisted_cpf}, your celphone is {persisted_celphone} and your account is {persisted_account_number}. \
-        #    How can I help you today? You can go to Safra Pay or Banking. Which service do you want?'.format(persisted_cpf=persisted_cpf, persisted_celphone=persisted_celphone, persisted_account_number=persisted_account_number)
-        #reprompt_text = 'How can I help you today? You can go to Safra Pay or Banking. Which service do you want?'
-        
-        #return (
-        #    handler_input.response_builder
-        #        .speak(speak_output)
-        #        .ask(reprompt_text)
-        #        .response
-        #)
-        
-        return AuthenticationIntentHandler.handle(handler_input)
-
-class AuthenticationIntentHandler(AbstractRequestHandler):
-    """Handler for Authentication Intent."""
-    #def can_handle(self, handler_input):
-    #    # type: (HandlerInput) -> bool
-    #    return true
-        
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        
         speak_output = 'Welcome back, your CPF is {persisted_cpf}, your celphone is {persisted_celphone} and your account is {persisted_account_number}. \
             How can I help you today? You can go to Safra Pay or Banking. Which service do you want?'.format(persisted_cpf=persisted_cpf, persisted_celphone=persisted_celphone, persisted_account_number=persisted_account_number)
         reprompt_text = 'How can I help you today? You can go to Safra Pay or Banking. Which service do you want?'
@@ -591,7 +569,6 @@ sb = CustomSkillBuilder(persistence_adapter=s3_adapter)
 
 sb.add_request_handler(HasClientInfoLaunchRequestHandler())
 sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(AuthenticationIntentHandler())
 sb.add_request_handler(BankingIntentHandler())
 sb.add_request_handler(AccountIntentHandler())
 sb.add_request_handler(CaptureCPFIntentHandler())
