62534203d50f1b6ec921cd6fd95e09b3ed731633
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-14 07:09:39 +0000

    updated lambda/capture_client_info.py.

--- lambda/capture_client_info.py
+++ lambda/capture_client_info.py
@@ -86,6 +86,9 @@ class CaptureCelphoneIntentHandler(AbstractRequestHandler):
         speak_output = 'Thanks, I will remember that your celphone is {celphone}. What\'s your account number?'.format(celphone=celphone)
         reprompt_text = 'What\'s your account number?'
         
+        # Add APL Template if device is compatible
+        apl_utils.capture_celphone_intent_screen(handler_input)
+        
         return (
             handler_input.response_builder
                 .speak(speak_output)
@@ -130,6 +133,9 @@ class CaptureAccountIntentHandler(AbstractRequestHandler):
             speak_output = 'Thanks, I\'ll remember that your account number is <say-as interpret-as=\"digits\">{account_number}</say-as>. \
                 To access our services, please confirm the token that was sent to your celphone.'.format(account_number=account_number)
             reprompt_text = 'Please, confirm the token we sent to your celphone.'
+            
+            # Add APL Template if device is compatible
+            apl_utils.capture_account_intent_screen(handler_input)
             
             return (
                 handler_input.response_builder
